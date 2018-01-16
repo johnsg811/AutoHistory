a603a3bce6df611f5c8255b37fd30d4c4c5f2c4c
Johns George <johnsg811@gmail.com>
2018-01-16 19:58:09 +0000

    commit

--- automobile/ViewController.swift
+++ automobile/ViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var Username: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func CreateAccount(_ sender: Any) {
+        Auth.auth().createUser(withEmail: Username.text!, password: Password.text!) { (user, error) in
+            
+            if error != nil{
+                self.login()
+            }else{
+                print("User Created")
+                self.login()
+            }
+        }
+        
+    }
+    
+    func login(){
+        Auth.auth().signIn(withEmail: Username.text!, password: Password.text!) { (user, error) in
+            
+            if error != nil{
+                print("Incorrect Username/Password")
+            }else{
+                print("Hello")
+            }
+        }
+    }
+    
 }
 
